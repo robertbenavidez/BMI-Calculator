3e4ad0c738c17d98854980eb09b6768c751b6978
Robert Benavidez <murabitbenavidez@gmail.com>
2021-02-28 10:23:07 -0500

    Updates

---
+++ precache-manifest.233f0691cff1a7e297b73720b8d98967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67523e7fc87a62f66d3ecd03ed473527",
+    "url": "/BMI-Calculator/index.html"
+  },
+  {
+    "revision": "c8e37682be73f479a560",
+    "url": "/BMI-Calculator/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "0ab5a571c27c4b0235a9",
+    "url": "/BMI-Calculator/static/css/main.e0a46aff.chunk.css"
+  },
+  {
+    "revision": "c8e37682be73f479a560",
+    "url": "/BMI-Calculator/static/js/2.82963dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BMI-Calculator/static/js/2.82963dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab5a571c27c4b0235a9",
+    "url": "/BMI-Calculator/static/js/main.0a4e7c2e.chunk.js"
+  },
+  {
+    "revision": "a2d6278272045e779f60",
+    "url": "/BMI-Calculator/static/js/runtime-main.0fb89217.js"
+  }
+]);
