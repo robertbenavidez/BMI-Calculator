4858a6ee0b7bf6aeb71df55928d109b327bb8740
Robert Benavidez <murabitbenavidez@gmail.com>
2021-02-27 21:32:25 -0500

    form fields working with reset

--- src/components/BMICalculator.js
+++ src/components/BMICalculator.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import FormInput from './FormInput'
 
 const BMICalculator = () => {
-    const onChangeInput = e => {}
+    const [heightUnit, setHeightUnit] = useState('cm');
+    const [weightUnit, setWeightUnit] = useState('kg');
+    const [unit, setUnit] = useState('Metric');
+    const [count, setCount] = useState({
+        heightCount: '0',
+        inchesCount: '0',
+        weightCount: '0',
+    });
+    // destructured Keys from count object
+    const { heightCount, inchesCount, weightCount} = count
+
+    useEffect(() => {
+        
+
+    }, [])
+
+    const onChangeInput = e => {
+        const {name, value} = e.target;
+        console.log(e.target)
+        setCount(prevState => ({...prevState, [name]: value}))
+    }
+
+    const onSelectTag = e => {
+        setUnit(e.target.value);
+        if (e.target.value === "Metric") {
+            setHeightUnit("cm");
+            setWeightUnit("kg");
+        } else {
+            setHeightUnit("ft");
+            setWeightUnit("lbs")
+        }
+    }
+
+    const resetFields = e => {
+        e.preventDefault();
+        setUnit('Metric');
+        setCount({
+            heightCount: '0',
+            inchesCount: '0',
+            weightCount: '0',
+        })
+        setHeightUnit("cm");
+        setWeightUnit("kg");
+    }
+
     return (
         <>
         <div className='bmi-inputs'>
@@ -12,7 +56,8 @@ const BMICalculator = () => {
                     <div className='unit'>
                         <select
                             name='unit'
-                            value=''
+                            value={unit}
+                            onChange={onSelectTag}
                             className='form-control form-control-sm'
                         >
                             <option value='Metric'>Metric</option>
@@ -23,27 +68,32 @@ const BMICalculator = () => {
                 <FormInput
                     type="text"
                     name="heightCount"
-                    title={`Height {cm}`}
-                    value=""
+                    title={`Height (${heightUnit})`}
+                    value={heightCount}
                     onChange={onChangeInput}
                  />
+                {
+                    unit === 'Imperial' ? 
+                    <FormInput
+                       type="text"
+                       name="inchesCount"
+                       title={` (in)`}
+                       value={inchesCount}
+                       onChange={onChangeInput}
+                    />
+                    : ''
+                }
+
                  <FormInput
                     type="text"
-                    name="inchesCount"
-                    title={`Height {inches}`}
-                    value=""
-                    onChange={onChangeInput}
-                 />
-                 <FormInput
-                    type="text"
-                    name="heightCount"
-                    title={`Weight {kg}`}
-                    value=""
+                    name="weightCount"
+                    title={`Weight (${weightUnit})`}
+                    value={weightCount}
                     onChange={onChangeInput}
                  />
                  
             </div>
-            <button className='button' type='submit'>
+            <button className='button' type='submit' onClick={resetFields}>
                 Reset
             </button>
         </div>
